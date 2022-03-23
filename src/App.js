2e0fea19391e643923ec04b009893e6cc766b911
Manfred Specht <pitch@specht.de>
2022-03-23 18:01:01 +0100

    adds logo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import './App.css';
+import "./App.css";
+import logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          SEITENSPENDER
-        </h1>
+        <img src={logo} alt="seitenspender logo" width="20%" />
+        <h1>SEITENSPENDER</h1>
         <p>
-        Welcome to SEITENSPENDER, the front-end library of choice for creating internet blogs.
+          Welcome to SEITENSPENDER, the front-end library of choice for creating
+          internet blogs.
         </p>
+        <p>You can clone or fork SEITENSPENDER on GitHub.</p>
         <p>
-          You can clone or fork SEITENSPENDER on GitHub.
-        </p>
-        <p>
-          <a href="https://github.com/mspkom/seitenspender" target="_blank" rel="noreferrer">SEITENSPENDER on GitHub</a>
+          <a
+            href="https://github.com/mspkom/seitenspender"
+            target="_blank"
+            rel="noreferrer"
+          >
+            SEITENSPENDER on GitHub
+          </a>
         </p>
       </header>
     </div>
